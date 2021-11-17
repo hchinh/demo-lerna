a28614e335c356966ef96e9cd9aa8ad6cee7bba8
Chinh Huynh <chinh.huynh@team.enouvo.com>
2021-11-17 18:29:56 +0700

    Initial commit

---
+++ packages/server-api/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Backend server");
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`Error: ${err}`);
+  } else {
+    console.log(`Listening on port ${port}`);
+  }
+});
